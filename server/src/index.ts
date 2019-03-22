0ff667e8a2b9845208878a85de4b46e115318ef0
Martin Wood <martin@martinwebdev.co.uk>
2019-03-23 01:44:21 +1100

    Martin customisation and package updates

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,11 @@
-import * as express from 'express';
+import * as express from "express";
 
 const server = express();
 const port = 8080;
+
+server.get("/", (req, res) => {
+    res.send("Hello World");
+});
 
 server.listen(port, () => {
     console.log(`Server is listening at http://localhost:${port}`);
